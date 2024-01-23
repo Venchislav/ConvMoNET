7a8c41ce0b6ff32f458c2c829fbd0e65e6b15769
Venchislav <97972996+Venchislav@users.noreply.github.com>
2024-01-23 21:18:43 +0300

    Ready for usage in Flask

---
+++ ML Model/model_ready_to_use.py
@@ -0,0 +1,41 @@
+from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.losses import SparseCategoricalCrossentropy
+from tensorflow.keras.optimizers import Adam
+from data_loader import categories
+
+
+IMG_SIZE = 28
+num_classes = len(categories)
+
+def create_model():
+    model = Sequential([
+        layers.Rescaling(1./255, input_shape=(IMG_SIZE, IMG_SIZE, 1)),
+        layers.Conv2D(30, (5, 5), activation='relu'),
+        layers.MaxPool2D(pool_size=(2, 2)),
+        layers.BatchNormalization(),
+        layers.Conv2D(15, (3, 3), activation='relu'),
+        layers.MaxPool2D(pool_size=(2, 2)),
+        layers.BatchNormalization(),
+
+        layers.Flatten(),
+
+        layers.Dense(128, activation='relu'),
+        layers.Dropout(0.2),
+        layers.Dense(64, activation='relu'),
+        layers.Dropout(0.3),
+        layers.Dense(num_classes, activation='softmax')
+    ])
+
+    model.compile(
+        optimizer=Adam(learning_rate=0.01),
+        loss=SparseCategoricalCrossentropy(),
+        metrics=['accuracy']
+    )
+
+    return model
+
+
+model = create_model()
+model.load_weights('E:\ConvMoNET\ML Model\model_saves')
+image = np.asarray(Image.open('E:/ConvMoNET/Data/bananas/4506024419524608-True.jpg').convert('L'))
