ee74413cb82db388320b2fed8130e15dad751a2c
Venchislav <97972996+Venchislav@users.noreply.github.com>
2024-01-28 21:42:06 +0300

    Finals

--- conv_monet.py
+++ conv_monet.py
@@ -1,5 +1,11 @@
-from flask import Flask
+from flask import Flask, request, render_template, jsonify
 from flask import render_template
+import base64, os, random
+from uuid import uuid4
+from MLModel.data_load import dec
+from tensorflow import keras
+from model import model, predict_
+import numpy as np
 
 app = Flask(__name__)
 
@@ -7,15 +13,26 @@ app = Flask(__name__)
 def main():
     return render_template('main_page.html')
 
-
-@app.route("/guesso")
-def guesso():
-    return "<p>GUESSO page!</p>"
-
-@app.route("/engenerate")
-def generate():
-    return "<p>enGENERATE page!</p>"
-
 @app.route("/info")
 def info():
-    return "<p>info page!</p>"
+    return render_template('info_page.html')
+
+@app.route("/guesso")
+def guesso_render():
+    return render_template('guesso_page.html')
+
+
+@app.route("/guesso", methods=["POST", "GET"])
+def predict_digit():
+    image = request.get_json(silent=True)['image'].split(",")[1]
+    image_data = base64.urlsafe_b64decode(image)
+
+
+    prediction = predict_(image_data)
+    print(np.argmax(prediction))
+
+    response = { 
+        "prediction": dec[np.argmax(prediction)]
+    }
+
+    return jsonify(response)
