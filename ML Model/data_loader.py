c3faf0e1dec9ae0b4ea07d51266dfa865d5da4ca
Venchislav <97972996+Venchislav@users.noreply.github.com>
2024-01-28 21:38:58 +0300

    Data Loader UPD

--- ML Model/data_loader.py
+++ ML Model/data_loader.py
@@ -11,15 +11,15 @@ def download(doodle_groups):
             os.mkdir(f'E:/ConvMoNET/Data/{group}s')
         except:
             continue
-        for i in range(500):
+        for i in range(10_000):
             drawing = qd.get_drawing(group[:-1])
             while drawing.recognized != True:
                 drawing = qd.get_drawing(group[:-1])
             drawing_img = drawing.get_image(stroke_color=(255, 255, 255), stroke_width=6, bg_color=(0, 0, 0))
             drawing_img.save(f'E:/ConvMoNET/Data/{group}s/{str(drawing.key_id) + "-" + str(drawing.recognized)}.jpg')
 
-categories = ['airplanes', 'apples', 'bananas', 'basketballs', 'bears', 'beds', 'bees', 'bicycles', 'birds', 'books', 'brains', 'breads', 'buckets', 'buss', 'cactuss', 'cakes', 'calculators', 'camels', 'cameras', 'campfires', 'candles', 'carrots', 'cars', 'castles', 'cats', 'chairs', 'churchs', 'circles', 'clocks', 'clouds', 'compasss', 'computers', 'cookies', 'couchs', 'cows', 'crabs', 'crocodiles', 'crowns', 'cups', 'diamonds', 'dogs', 'dolphins', 'doors', 'dragons', 'dumbbells', 'elephants', 'eyeglassess', 'eyes', 'faces', 'feathers', 'fences', 'fireplaces', 'fishs', 
-'flowers', 'flying saucers', 'frogs', 'frying pans', 'giraffes', 'grapess', 'grasss', 'guitars', 'hamburgers', 'hammers', 'hats', 'headphoness', 'hedgehogs', 'helicopters', 'horses', 'hot air balloons', 'hot dogs', 'house plants', 'houses', 'ice creams', 'keys', 'knifes', 'laptops', 'leafs', 'light bulbs', 'lightnings', 'lions', 'lollipops', 'maps', 'microphones', 'monkeys', 'mountains', 'mushrooms', 'oceans', 'octopuss', 'onions', 'owls', 'palm trees', 'pandas', 'pantss', 'pears', 'pencils', 'pigs', 'pineapples', 'pizzas', 'potatos', 'power outlets', 'purses', 'rabbits', 'radios', 'remote controls', 'rifles', 'sailboats', 'sandwichs', 'saws', 'scissorss', 'sea turtles', 'sheeps', 'shoes', 'shortss', 'shovels', 'skateboards', 'skulls', 'smiley faces', 'snails', 'snakes', 'snowflakes', 'snowmans', 'soccer balls', 'spiders', 'squares', 
-'stars', 'stitchess', 'stop signs', 'strawberrys', 'submarines', 'suitcases', 'suns', 'swans', 'swords', 't-shirts', 'tables', 'teddy-bears', 'telephones', 'televisions', 'tents', 'The Eiffel Towers', 'The Mona Lisas', 'tigers', 'toilets', 'tooths', 'tractors', 'traffic lights', 'trees', 'triangles', 'trucks', 'umbrellas', 'underwears', 'watermelons', 'whales', 'wheels', 'windmills', 'wristwatchs', 'zigzags']
-
+categories = ['apples', 'basketballs', 'cakes', 'cameras', 'cars', 'cats',
+              'clocks', 'cookies', 'crowns', 'cups', 'eyeglassess', 'faces', 'house plants',
+              'houses', 'ice creams', 'keys', 'lollipops', 'mushrooms', 'pizzas', 'skulls', 
+              'windmills']
 # download(categories)
